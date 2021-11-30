e12b31ce695ab0afe22db7ed05b190fced14a1f9
Kai Havlik <kai@havlik.ch>
2021-11-30 18:59:39 +0100

    added feed page

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import FeedPage from './pages/FeedPage';
 
 const Router = (props) => {
   // const {} = props;
@@ -12,6 +13,7 @@ const Router = (props) => {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/feed" element={<FeedPage />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/FeedPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// const TogglePassword = (props) => {
+//     const password = document.querySelector('#inputPassword');
+//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+          
+//     password.setAttribute('type', type);
+
+//     this.classList.toggle('bi-eye');
+// };
+
+const FeedPage = (props) => {
+  // const {} = props;
+  return (
+    <>
+    </>
+  );
+};
+
+export default FeedPage;
