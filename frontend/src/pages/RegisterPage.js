999a21bde196465c17d29b36b90fd85c6f59e04e
Kai Havlik <kai@havlik.ch>
2021-12-03 18:41:49 +0100

    setup axios request

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,40 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faEye } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+const url = "http://localhost:8888/index.php?_url=userservice/register";
 
 const RegisterPage = (props) => {
-  // const {} = props;
+
+  const [formData, setformData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordRepeat: '',
+    terms: ''
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const registerFormData = new FormData();
+    registerFormData.append("username", formData.username)
+    registerFormData.append("email", formData.email)
+    registerFormData.append("password", formData.password)
+    registerFormData.append("passwordRepeat", formData.passwordRepeat)
+    
+    // console log registerFormData
+    for (var pair of registerFormData.entries()) {
+      console.log(pair[0]+ ', ' + pair[1]); 
+    }
+    
+
+    axios({
+      method: "post",
+      url: url,
+      data: registerFormData,
+      headers: { 
+        "Content-Type": "multipart/form-data"
+      },
+    })
+      .then(function (res) {
+        //handle success
+        console.log(res);
+      })
+      .catch(function (res) {
+        //handle error
+        console.log(res);
+      });
+  }
+
+  const handleChange = (e) => {
+    setformData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
   return (
     <div id="register-page" className="col align-self-center justify-self-center">
-      <form className="form-container d-flex flex-column m-auto">
-          <div className="card">
-              <div className="card-body">
-                  <h3 className="text-center">register now</h3>
-                  <div className="mb-3">
-                      <label for="InputUsername" className="form-label">Username</label>
-                      <input type="text" className="form-control" id="InputUsername" aria-describedby="usernameHelp"/>
-                  </div>
-                  <div className="mb-3">
-                      <label for="inputEmail" className="form-label">Email address</label>
-                      <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp"/>
-                  </div>
-                  <div className="mb-3">
-                      <label for="inputPassword" className="form-label">Password</label>
-                      <input type="password" className="form-control" id="inputPassword"/>
-                  </div>
-                  <div className="mb-3">
-                      <label for="inputPasswordRepeat" className="form-label">Password repeat</label>
-                      <input type="password" className="form-control" id="inputPasswordRepeat"/>
-                  </div>
-                  <div className="mb-3 form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" disabled/>
-                      <label className="form-check-label" for="exampleCheck1">Accept our terms of service</label>
-                  </div>
-              </div>
-          </div>
-          <button type="submit" className="btn btn-primary-circle" name="submit">
-            <FontAwesomeIcon icon={faArrowRight} />          
-          </button>
+      <form className="form-container d-flex flex-column m-auto" onSubmit={handleSubmit}>
+        <div className="card">
+            <div className="card-body">
+                <h3 className="text-center">register</h3>
+                <div className="mb-3">
+                    <label htmlFor="InputUsername" className="form-label">Username</label>
+                    <input type="text" className="form-control" id="InputUsername" name="username" value={formData.username} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="inputEmail" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="inputEmail" name="email" value={formData.email} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="inputPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="inputPassword" name="password" value={formData.password} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="inputPasswordRepeat" className="form-label">Password repeat</label>
+                    <input type="password" className="form-control" id="inputPasswordRepeat" name="passwordRepeat" value={formData.passwordRepeat} onChange={handleChange}/>
+                </div>
+                {/* <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="checkTerms"/>
+                    <label className="form-check-label" htmlFor="checkTerms" name="terms">I accept our <a href="/">Terms of Use</a> & <a href="/">Privacy Policy</a></label>
+                </div> */}
+            </div>
+        </div>
+        <button type="submit" className="btn btn-primary-circle" name="submit">
+          <FontAwesomeIcon icon={faArrowRight} />          
+        </button>
       </form>
     </div>
   );
