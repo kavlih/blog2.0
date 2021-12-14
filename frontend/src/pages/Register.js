90c84378aac6b63ffe5a69506dd640a5f91f0050
Kai Havlik <kai@havlik.ch>
2021-12-14 22:26:03 +0100

    sign up form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash, faXmark } from "@fortawesome/free-regular-svg-icons";
 
 import FormInput from '../components/FormInput';
 
@@ -20,7 +21,6 @@ const formSettings = {
     },
     password: {
         minLenght: 8,
-        maxLenght: 64,
         // no whitespaces
         // 1 upper- and lowercase char
         // 1 number
@@ -31,7 +31,6 @@ const formSettings = {
 
 // TODO if username already exists
 // TODO if email is already registered
-// TODO password tooltip
 // TODO Email verification
 
 const Register = (props) => {
@@ -44,12 +43,14 @@ const Register = (props) => {
         password: '',
     };
     
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState(initialValues);
-    const [stateVal, setStateVal] = useState(initialValues);
+    const [formValues, setFormValues]   = useState(initialValues);
+    const [formErrors, setFormErrors]   = useState(initialValues);
+    const [stateVal, setStateVal]       = useState(initialValues);
+    const [isFocus, setIsFocus]         = useState(initialValues);
 
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
     const [isSubmit, setIsSubmit] = useState(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
     useEffect(() => {
         setIsButtonDisabled(stateVal.username && stateVal.email && stateVal.password ? false : true)
@@ -60,6 +61,23 @@ const Register = (props) => {
         setFormValues({...formValues, [name]: value}); 
         setFormErrors(formValidate(name, value));
     };
+
+    const handleBlur = (e) => {
+        setIsFocus({[e.target.name]: false});
+    };
+    
+    const handleFocus = (e) => {
+        const {name, value} = e.target;
+        setIsFocus({[name]: true});
+        setFormValues({...formValues, [name]: value}); 
+        setFormErrors(formValidate(name, value));
+    };
+  
+    const handleMouseDown = (e) => {
+        // keeps focus on input
+        e.preventDefault();
+        setIsPasswordVisible(isPasswordVisible ? false : true);
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -101,7 +119,7 @@ const Register = (props) => {
         const regexUsername = formSettings.username.regex;
         const regexPassword = formSettings.password.regex;
 
-        const errors = {...formErrors, [name]: ""};
+        let errors = {...formErrors, [name]: []};
 
         switch (name) {
             case "username":
@@ -119,23 +137,27 @@ const Register = (props) => {
             case "email":
                 if (!value) {
                     errors[name] = "Please type in an email";
-                } else if (!regexEmail.test(value)) {
+                } 
+                else if (!regexEmail.test(value)) {
                     errors[name] = "Email is invalid";
                 }
                 break;
 
             case "password":
                 if (!value) {
-                    errors[name] = "Password is required";
+                    errors[name].push("empty");
                 } 
-                else if (value.length < formSettings.password.minLenght) {
-                    errors[name] = `Password requires minimum ${formSettings.password.minLenght} characters`;
+                if (value.length < formSettings.password.minLenght) {
+                    errors[name].push("lenght");
                 }
-                else if (value.length > formSettings.password.maxLenght) {
-                    errors[name] = `Password should be maximum ${formSettings.password.maxLenght} characters long`;
+                if (!value.match(/\d/)) {
+                    errors[name].push("number");
                 }
-                else if (!regexPassword.test(value)) {
-                    errors[name] = "Password is invalid";
+                if (!value.match(/\W/)) {
+                    errors[name].push("special");
+                }
+                if (!value.match(/[A-Z]/) || !value.match(/[a-z]/)) {
+                    errors[name].push("letter");
                 }
                 break;
         }
@@ -155,36 +177,91 @@ const Register = (props) => {
             <form className="d-flex flex-column m-auto" onSubmit={handleSubmit} noValidate>
                 <div className="card">
                     <div className="card-body">
-                        <h3 className="text-center">SignUp</h3>
-                        <FormInput
-                            type="text"
-                            id="inputUsername"
-                            name="username"
-                            label="Username"
-                            value={formValues.username}
-                            errors={formErrors.username}
-                            onChange={handleChange}
-                            focus={true}
-                        />
-                        <FormInput
-                            type="email"
-                            id="inputEmail"
-                            name="email"
-                            label="Email address"
-                            value={formValues.email}
-                            errors={formErrors.email}
-                            onChange={handleChange}
-                        />
-                        <FormInput
-                            type="password"
-                            id="inputPassword"
-                            name="password"
-                            label="Password"
-                            value={formValues.password}
-                            errors={formErrors.password}
-                            onChange={handleChange}
-                        />
-                        <p className="text-center">By signing up you agree to our <a href="">Terms of Use</a> & <a href="">Privacy Policy</a>.</p>
+                        <h3 className="text-center">Sign Up</h3>
+                        <div className="m-input-container mb-3">
+                            {/* label */}
+                            <label htmlFor="username" className="form-label mb-0">Username</label>
+                            {/* info */}
+                            <small>
+                                <p className="m-info mb-2">{`${formSettings.username.minLenght}-${formSettings.username.maxLenght} characters. Only letters or numbers`}</p>
+                            </small>
+                            {/* input group */}
+                            <div className="m-input-group">
+                                {/* input */}
+                                <input 
+                                    type="text"
+                                    className={`form-control ${formErrors.username.length > 0 && !isFocus.username ? "is-invalid" : ""}`} 
+                                    id="inputUsername" 
+                                    name="username"
+                                    value={formValues.username} 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur} 
+                                    onFocus={handleFocus} 
+                                    autoFocus={true}
+                                />
+                                {/* errors */}
+                                {formErrors.username.length > 0 && !isFocus.username && <div className="invalid-feedback">{formErrors.username}</div>}
+                            </div>
+                        </div>
+                        <div className="m-input-container mb-3">
+                            {/* label */}
+                            <label htmlFor="email" className="form-label">Email</label>
+                            {/* input group */}
+                            <div className="m-input-group">
+                                {/* input */}
+                                <input 
+                                    type="email"
+                                    className={`form-control ${formErrors.email.length > 0 && !isFocus.email ? "is-invalid" : ""}`} 
+                                    id="inputEmail" 
+                                    name="email"
+                                    value={formValues.email} 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur}
+                                    onFocus={handleFocus} 
+                                />
+                                {/* errors */}
+                                {formErrors.email.length > 0 && !isFocus.email && <div className="invalid-feedback">{formErrors.email}</div>}
+                            </div>
+                        </div>
+                        <div className="m-input-container mb-3">
+                            {/* label */}
+                            <label htmlFor="password" className="form-label">Password</label>
+                            {/* input group */}
+                            <div className="m-input-group">
+                                {/* input */}
+                                <input 
+                                    type={isPasswordVisible ? "text" : "password"}
+                                    className={`form-control ${Object.keys(formErrors.password).length > 0 && !isFocus.password ? "is-invalid" : ""}`} 
+                                    id="inputPassword" 
+                                    name="password"
+                                    value={formValues.password} 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur} 
+                                    onFocus={handleFocus} 
+                                />
+                                {/* input button */}
+                                <button 
+                                    className="btn position-absolute m-toggle-password" 
+                                    type="button" 
+                                    id="button-addon2"
+                                    tabIndex="-1"
+                                    onMouseDown={handleMouseDown}
+                                >
+                                    <FontAwesomeIcon icon={isPasswordVisible ? faEyeSlash : faEye} />
+                                </button>
+                                {/* errors */}
+                                {Object.keys(formErrors.password).length > 0 && <small className="m-password-conditions">
+                                    <ul className="list-group">
+                                        <li className={`${!formErrors.password.includes("lenght")    ? "valid" : ""}`}>• Use 8 or more characters</li>
+                                        <li className={`${!formErrors.password.includes("letter")    ? "valid" : ""}`}>• Use upper and lower case characters</li>
+                                        <li className={`${!formErrors.password.includes("number")    ? "valid" : ""}`}>• Use a number</li>
+                                        <li className={`${!formErrors.password.includes("special")   ? "valid" : ""}`}>• Use a speacial character</li>
+                                    </ul>
+                                </small>}
+                                <div className="invalid-feedback">{`${formErrors.password.includes("empty") ? "Please type in a password" : "Password is invalid"}`}</div>
+                            </div>
+                        </div>
+                        <p className="m-terms text-center"><small>By signing up you agree to our <br /><a href="/">Terms of Use</a> & <a href="/">Privacy Policy</a>.</small></p>
                     </div>
                 </div>
                 <button type="submit" className="btn m-btn m-btn-green" name="submit" disabled={isButtonDisabled}>
