372d3c770ec5650bbe0cf36731fbfd5cbbd274a7
Kai Havlik <kai@havlik.ch>
2021-12-20 15:52:13 +0100

    login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,49 +1,145 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+import { accountService } from '../_services';
+// FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { faEye } from "@fortawesome/free-regular-svg-icons";
-
-// const TogglePassword = (props) => {
-//     const password = document.querySelector('#inputPassword');
-//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-          
-//     password.setAttribute('type', type);
-
-//     this.classList.toggle('bi-eye');
-// };
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 
 const Login = (props) => {
+  const initialValues = {
+    user: '',
+    password: '',
+  };
+
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState(initialValues);
+  const [stateVal, setStateVal]     = useState(initialValues);
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  useEffect(() => {
+    setIsButtonDisabled(stateVal.user && stateVal.password ? false : true)
+  }, [stateVal]);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]: value}); 
+    setFormErrors(formValidate(name, value));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const fields = new FormData();
+    fields.append("user", formValues.user)
+    fields.append("password", formValues.password)
+    
+    accountService.login(fields)
+    .then(() => {
+      console.log("yaaaaaaaay");
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+  };
+
+  const handleMouseDown = (e) => {
+    // keeps focus on input
+    e.preventDefault();
+    setIsPasswordVisible(isPasswordVisible ? false : true);
+  }
+
+  const formValidate = (name, value) => {
+    const errors = {...formErrors, [name]: ""};
+
+    switch (name) {
+      case "user":
+        if (!value) {
+          errors[name] = "Please type in a username or an email address";
+        }
+        break;
+
+      case "password":
+        if (!value) {
+            errors[name] = "Password is required";
+        }
+        break;
+        
+      default:
+        break;
+    }
+
+    if(value && errors[name].length === 0) {
+        setStateVal({...stateVal, [name]: true})
+    }
+    else {
+        setStateVal({...stateVal, [name]: false})
+    }
+
+    return errors;
+};
+
   return (
     <div id="login-page" className="col align-self-center justify-self-center">
-      <form className="form-container d-flex flex-column m-auto">
-          <div className="card">
-              <div className="card-body d-flex flex-column">
-                  <h3 className="text-center">welcome back :)</h3>
-                  <div className="mb-3">
-                      <label htmlFor="inputUser" className="form-label">Username or email</label>
-                      <input type="text" className="form-control" id="inputUser"/>
-                  </div>
-                  <div className="mb-3">
-                    <div className="form-label d-flex justify-content-between">
-                        <label htmlFor="inputPassword">Password</label>
-                        <a href="/">Forgot password?</a>
-                      </div>
-                      <div className="input-group">
-                        <input type="password" className="form-control" id="inputPassword"/>
-                        <button className="btn position-absolute togglePassword" type="button" id="button-addon2" disabled>
-                          <FontAwesomeIcon icon={faEye} />
-                        </button>
-                      </div>
-                  </div>
-                  {/* <div className="mb-3 form-check">
-                      <input type="checkbox" className="form-check-input" id="checkLogin" disabled/>
-                      <label className="form-check-label" htmlFor="checkLogin">stay logged in</label>
-                  </div> */}
-              </div>
+      {/* <div className="alert alert-success alert-dismissible fade show" role="alert">
+        You signed up successfully.
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div> */}
+      <form className="form-container d-flex flex-column" onSubmit={handleSubmit} noValidate>
+        <div className="card">
+          <div className="card-body">
+            <h3 className="text-center">Login</h3>
+            <div className="m-input-container mb-3">
+                {/* label */}
+                <label htmlFor="username" className="form-label">Username</label>
+                <div className="m-input-group">
+                    {/* input */}
+                    <input 
+                        type="text"
+                        className="form-control" 
+                        id="inputUser"
+                        name="user"
+                        value={formValues.user} 
+                        onChange={handleChange} 
+                        autoFocus={true}
+                    />
+                </div>
+            </div>
+            <div className="m-input-container mb-3">
+                {/* label */}
+                <label htmlFor="password" className="form-label">Password</label>
+                {/* input group */}
+                <div className="m-input-group">
+                    {/* input */}
+                    <input 
+                        type={isPasswordVisible ? "text" : "password"}
+                        className="form-control" 
+                        id="inputPassword" 
+                        name="password"
+                        value={formValues.password} 
+                        onChange={handleChange} 
+                    />
+                    {/* input button */}
+                    <button 
+                        className="btn position-absolute m-toggle-password" 
+                        type="button" 
+                        id="button-addon2"
+                        tabIndex="-1"
+                        onMouseDown={handleMouseDown}
+                    >
+                        <FontAwesomeIcon icon={isPasswordVisible ? faEyeSlash : faEye} />
+                    </button>
+                    {/* errors */}
+                    <div className="invalid-feedback">{`${formErrors.password.includes("empty") ? "Please type in a password" : "Password is invalid"}`}</div>
+                </div>
+            </div>
           </div>
-          <button type="submit" className="btn btn-primary-circle" name="submit">
-            <FontAwesomeIcon icon={faArrowRight} />          
-          </button>
+        </div>
+        <button type="submit" className="btn m-btn m-btn-green" name="submit" disabled={isButtonDisabled}>
+          <FontAwesomeIcon icon={faArrowRight} />          
+        </button>
       </form>
     </div>
   );
