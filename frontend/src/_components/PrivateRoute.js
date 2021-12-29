ae074d549b44af9466d77024ae097ec336f70ed7
Kai Havlik <kai@havlik.ch>
2021-12-29 17:34:43 +0100

    temp way to authorize user

--- frontend/src/_components/PrivateRoute.js
+++ frontend/src/_components/PrivateRoute.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-// import { accountService } from '../_services';
-
 const PrivateRoute = () => {
   const auth = useAuth();
   return auth ? <Outlet /> : <Navigate to="/account/login" />;
 }
 
 const useAuth = () => {
-  return true;
+  const loggedInUser = localStorage.getItem("user");
+  return loggedInUser;
 }
 
 export { PrivateRoute };
--- frontend/src/account/Index.js
+++ frontend/src/account/Index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import { Login } from './Login';
 import { Register } from './Register';
 
 const Account = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+
+    if (loggedInUser) {
+      navigate("/feed");
+    }
+  }, []);
 
   return (
     <div className="m-account col align-self-center justify-self-center">
