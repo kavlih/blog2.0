dd4ccf618a8096495a54670da76149e8428edef9
Kai Havlik <kai@havlik.ch>
2021-12-29 17:31:25 +0100

    rm nav collapse & setup second nav

--- frontend/src/_components/Nav.js
+++ frontend/src/_components/Nav.js
@@ -1,47 +1,50 @@
-import React from 'react';
+// TODO Nav doesn't get rerendered after redirect from login
+
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = (props) => {
-  // const {} = props;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    setUser(loggedInUser)
+  }, []);
+
   return (
     <nav className="m-nav navbar navbar-expand-lg">
-      <div className="container-fluid">
+      {/* if user is logged in */}
+      {user && <div className="container-fluid">
         <div id="logo">
-          <Link className="nav-link" to="/">
-            Logo
-          </Link>
+          <Link className="nav-link col-1" to="/">Logo</Link>
         </div>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNavAltMarkup" 
-          aria-controls="navbarNavAltMarkup" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+        <div className="justify-content-center">
           <div className="navbar-nav">
-            {/* if user is NOT logged in */}
-            <Link className="nav-link" to="/account/login">
-              Login
-            </Link>
-            <Link className="nav-link" to="/account/register">
-              Register
-            </Link>
-            {/* if user IS logged in */}
-            {/* <Link className="nav-link" to="/feed">
-              Feed
-            </Link> */}
+            <Link className="nav-link" to="/feed">Feed</Link>
+            <Link className="nav-link" to="/">Channels</Link>
+            <Link className="nav-link" to="/">Profile</Link>
+            <Link className="nav-link" to="/">Users</Link>
           </div>
         </div>
         <div>
-          <Link className="nav-link" to="/">
-            Settings
-          </Link>
+          <Link className="nav-link col-1" to="/">Settings</Link>
         </div>
-      </div>
+      </div>}
+      {/* if user is NOT logged in */}
+      {!user && <div className="container-fluid">
+      <div id="logo" className="col-1">
+          <Link className="nav-link" to="/">Logo</Link>
+        </div>
+        <div className="justify-content-center">
+          <div className="navbar-nav">
+            <Link className="nav-link" to="/account/login">Login</Link>
+            <Link className="nav-link" to="/account/register">Register</Link>
+          </div>
+        </div>
+        <div className="col-1">
+          {/* Placeholder */}
+        </div>
+      </div>}
     </nav>
   );
 };
