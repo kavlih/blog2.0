c969fadcde6ba7c95956d10fd7af1793e385487b
Kai Havlik <kai@havlik.ch>
2021-12-29 17:33:57 +0100

    store user in local storage

--- frontend/src/_services/account.service.js
+++ frontend/src/_services/account.service.js
@@ -1,10 +1,19 @@
+// TODO get users data after login from database
+// TODO store users data in a useContext hook after successful login
+
 import { axiosHelper } from '../_helpers';
 
 const config = "http://localhost:8888/index.php?_url=";
 const baseUrl = `${config}userservice`;
 
 const login = (data) => {
+  // returns promise
   return axiosHelper.post(`${baseUrl}/login`, data)
+    .then((res) => {
+      // store user in localStorage
+      localStorage.setItem("user", JSON.stringify(res.data));
+      return res;
+    })
 }
 
 const register = (data) => {
@@ -13,5 +22,5 @@ const register = (data) => {
 
 export const accountService = {
   login,
-  register
+  register,
 };
