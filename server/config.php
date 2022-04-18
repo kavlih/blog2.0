5fc7db4e5bf18dc8143c7b4e06fbb9cca66ff10d
Kai Havlik <kai@havlik.ch>
2022-04-18 19:20:33 +0200

    removed define tokens

--- server/config.php
+++ server/config.php
@@ -10,14 +10,5 @@ define('DB_CHARSET', 'utf8');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 
-// Token
-define('JWT_ALGO', 'HS256');
-define('JWT_AUD', 'http://localhost:8888');
-define('JWT_ISS', 'http://localhost:8888');
-define('JWT_SECRET', 'Qw!fwzjP^L@3LV&p8fi$U4t5^D$@GhDy89An6@K4H4W5%phb&$');
-
-// Directories
-
-
 // Error reporting
 define('DEBUG_MODE', TRUE);
