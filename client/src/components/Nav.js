1d6331569a632e2aba37a61d821042da1d37bdd0
Kai Havlik <kai@havlik.ch>
2022-04-20 00:12:59 +0200

    new nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,127 +1,201 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 // MUI Components
+import { styled } from "@mui/material";
 import { makeStyles } from '@mui/styles';
-import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import Stack from '@mui/material/Stack';
 // MUI Icons
+import MenuIcon from '@mui/icons-material/Menu';
 import SettingsIcon from '@mui/icons-material/Settings';
 
 import { UserContext } from '../context/UserContext';
 
-const useStyles = makeStyles((theme) => ({
-  nav: {
-    position: 'relative',
-    '& a': {
-      fontSize: theme.typography.h5.fontSize,
-      fontFamily: theme.typography.h5.fontFamily,
-      fontWeight: theme.typography.h5.fontWeight,
-      color: theme.palette.text.secondary,
-      textDecoration: 'none',
-      '&.active, &:hover': {
-        color: theme.palette.text.primary,
-      },
-      '&:before': {
-        content: "'>'",
-        marginRight: '4px',
-        opacity: 0
-      },
-      '&.active:before, &:hover:before': {
-        content: "'>'",
-        marginRight: '4px',
-        opacity: 1
-      }
-    },
-    '& button': {
-      position: 'absolute',
-      right: 0,
-    }
+const StyledButton = styled(Button)(({ theme }) => ({
+  fontSize: theme.typography.h5.fontSize,
+  fontFamily: theme.typography.h5.fontFamily,
+  fontWeight: theme.typography.h5.fontWeight,
+
+  '&:before': {
+    color: theme.palette.text.primary,
   },
-  paper: {
-    '& .MuiMenuItem-root': {
-      '& a': {
-        color: theme.palette.text.primary,
-        textDecoration: 'none',
-      },
-      fontFamily: theme.typography.body2.fontFamily
-    }
+
+  '&:hover': {
+    backgroundColor: 'transparent'
+  },
+
+  '& a': {
+    color: theme.palette.text.secondary,
+  },
+ 
+  '&:hover a, & a.active': {
+    color: theme.palette.text.primary,
   }
-}))
 
-const NavMenu = () => {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+}));
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+const Nav = () => {
+  const { user } = useContext(UserContext);
+  const [ auth, setAuth ] = useState(user);
+  const [ anchorElSettings, setAnchorElSettings ] = useState(null);
+  const [ anchorElNav, setAnchorElNav ] = useState(null);
+
+  useEffect(() => {
+    setAuth(user);
+  }, [user])
+
+  const handleSettingsOpen = (event) => {
+    setAnchorElSettings(event.currentTarget);
+  };
+  
+  const handleSettingsClose = () => {
+  setAnchorElSettings(null);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleNavMenuOpen = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleNavMenuClose = () => {
+    setAnchorElNav(null);
   };
 
   const navigate = useNavigate();  
   const handleLogout = () => {
     localStorage.clear();
-    handleClose();
+    handleSettingsClose();
     navigate('/account/login');
   };
 
-  return (
-    <>
-      <IconButton
-        id='basic-button'
-        aria-label='more'
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup='true'
-        onClick={handleClick}
-      >
-        <SettingsIcon />
-      </IconButton>
-      <Menu
-        id='basic-menu'
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        className={classes.paper}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}><NavLink to='/settings'>Settings</NavLink></MenuItem>
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-      </Menu>
-    </>
+  const renderSettings = (
+    <Menu
+      id="settings-appbar"
+      anchorElSettings={anchorElSettings}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={Boolean(anchorElSettings)}
+      onClose={handleSettingsClose}
+    >
+      <MenuItem onClick={handleSettingsClose}>
+        <NavLink to='/settings'>Settings</NavLink>
+      </MenuItem>
+      <MenuItem onClick={handleLogout}>
+        Logout
+      </MenuItem>
+    </Menu>
   );
-}
-
-const Nav = () => {
-  const classes = useStyles();
-  const { user } = useContext(UserContext);
 
   return (
-    <Container>
-      <Stack 
-        spacing={4} 
-        direction='row' 
-        justifyContent='center' 
-        alignItems='center' 
-        pt={6}
-        pb={10}
-        className={classes.nav}
-      >
-        <NavLink to='/feed' style={{marginLeft: '-10px'}}>Feed</NavLink>
-        <NavLink to='/profile'>Profile</NavLink>
-        <NavLink to='/users'>Users</NavLink>
-        {user && <NavMenu />}
-      </Stack>
-    </Container>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color='transparent'>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters component='nav'>
+            {/* Logo */}
+            <Typography sx={{ mr: 2, display: { xs: 'none', sm: 'block' }, position:'absolute', left: 0 }}
+              variant="h4"
+              noWrap
+              component="div"
+            >
+              whoosh
+            </Typography>
+            {/* Mobile Navigation */}
+            <Box sx={{ display: { xs: 'block', sm: 'none' }, position:'absolute', left: 0 }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleNavMenuOpen}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorElSettings={anchorElNav}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                keepMounted
+                transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+                open={Boolean(anchorElNav)}
+                onClose={handleNavMenuClose}
+                sx={{ display: { xs: 'block', sm: 'none' } }}
+              >
+                <MenuItem onClick={handleNavMenuClose}>
+                  <Typography textAlign="center">Feed</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleNavMenuClose}>
+                  <Typography textAlign="center">Profile</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleNavMenuClose}>
+                  <Typography textAlign="center">Users</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+            {/* Navigation */}
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' }, justifyContent:'center', alignItems:'center' }}>
+              <StyledButton onClick={handleNavMenuClose} fixedArrow sx={{ ml:'-13px' }}>
+                <NavLink to='/feed'>Feed</NavLink>
+              </StyledButton>
+              <StyledButton onClick={handleNavMenuClose} fixedArrow>
+                <NavLink to='/profile'>Profile</NavLink>
+              </StyledButton>
+              <StyledButton onClick={handleNavMenuClose} fixedArrow>
+                <NavLink to='/users'>Users</NavLink>
+              </StyledButton>
+            </Box>
+            {/* Molbile Logo */}
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
+              <Typography sx={{ flexGrow: 1, display:'flex', justifyContent:'center', alignItems:'center' }}
+                variant="h4"
+                noWrap
+                component="div"
+              >
+                whoosh
+              </Typography>
+            </Box>
+            {/* Settings */}
+            {auth && (
+              <Box sx={{ position:'absolute', right: 0 }}>
+                {/* <IconButton
+                  size="large"
+                  aria-label="show 17 new notifications"
+                  color="inherit"
+                >
+                  <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton> */}
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-haspopup="true"
+                  onClick={handleSettingsOpen}
+                  color="inherit"
+                >
+                  <SettingsIcon />
+                </IconButton>
+              </Box>
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {renderSettings}
+    </Box>
   );
 };
-
 export default Nav;
